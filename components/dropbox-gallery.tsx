82e04782637c34040dc185b1e5b67cb6d3080809
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-10-20 12:36:59 +0800

    cache token

--- components/dropbox-gallery.tsx
+++ components/dropbox-gallery.tsx
@@ -12,49 +12,82 @@ export function DropboxGalleryComponent() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null as string | null)
   const [hasMore, setHasMore] = useState(true)
-
-  // curl https://api.dropbox.com/oauth2/token \
-  //   -d grant_type=refresh_token \
-  //   -d refresh_token=<REFRESH_TOKEN> \
-  //   -d client_id=<APP_KEY> \
-  //   -d client_secret=<APP_SECRET>
-
-
-  let dbx = useRef(null as Dropbox | null).current
   
+  let dbx = useRef(null as Dropbox | null).current
   const limit = 3
   const imagesMetadataRef = useRef([] as files.FileMetadata[])
   const [start, setStart] = useState(0)
+  const accessTokenRef = useRef<string | null>(null)
 
-  const loadImagesLazy = useCallback(async () => {
 
-    if (!hasMore) {
-      return
+  const getAccessToken = useCallback(async () => {
+    if (accessTokenRef.current) return accessTokenRef.current
+
+    // local storage
+    const accessToken = localStorage.getItem('dropboxAccessToken')
+    if (accessToken) {
+      accessTokenRef.current = accessToken
+      return accessToken
     }
 
+    const response = await fetch('https://api.dropbox.com/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: process.env.NEXT_PUBLIC_DROPBOX_REFRESH_TOKEN ?? '',
+        client_id: process.env.NEXT_PUBLIC_DROPBOX_APP_KEY ?? '',
+        client_secret: process.env.NEXT_PUBLIC_DROPBOX_APP_SECRET ?? '',
+      }),
+    })
+
+    const data = await response.json()
+    if (response.ok) {
+      accessTokenRef.current = data.access_token
+      localStorage.setItem('dropboxAccessToken', data.access_token)
+      return data.access_token
+    } else {
+      throw new Error('Failed to get access token')
+    }
+  }, [])
+
+  const handleApiError = useCallback(async (err: any, retryFn: Function) => {
+    if (err.status === 401) {
+      console.log('Token expired. Refreshing...')
+      accessTokenRef.current = null // Clear cached token
+      localStorage.removeItem('dropboxAccessToken')
+      const token = await getAccessToken()
+      dbx = new Dropbox({ accessToken: token })
+      await retryFn() // Retry the original operation
+    } else {
+      throw err // Handle other errors
+    }
+  }, [getAccessToken])
+
+  const loadImagesLazy = useCallback(async () => {
+    if (!hasMore) return
     if (imagesMetadataRef.current.length === 0) {
-      console.log("loadImagesMetadata")
       await loadImagesMetadata()
     }
 
     try {
-      console.log("imagesMetadata", imagesMetadataRef.current)
       const imagesMetadataCut = imagesMetadataRef.current.slice(start, start + limit)
       setStart(start + limit)
-      
-      console.log(`start: ${start}, limit: ${limit}, imagesMetadataCut: ${imagesMetadataCut.length}, total: ${imagesMetadataRef.current.length}`)
 
       const imageUrls = await Promise.all(
         imagesMetadataCut.map(async (file) => {
-          const response = await dbx?.filesGetTemporaryLink({ path: file.path_lower ?? '' })
-          console.log("response", response);
-          if (!response) {
-            throw new Error('Failed to get temporary link')
+          try {
+            const response = await dbx?.filesGetTemporaryLink({ path: file.path_lower ?? '' })
+            const result = response?.result
+            return { url: result?.link ?? '', name: file.name }
+          } catch (err) {
+            await handleApiError(err, () => dbx?.filesGetTemporaryLink({ path: file.path_lower ?? '' }))
           }
-          const result = response.result
-          return { url: result.link, name: file.name }
         })
       )
+
       setImages(prevImages => [...prevImages, ...imageUrls])
       setHasMore(imagesMetadataRef.current.length > start)
     } catch (err) {
@@ -63,37 +96,22 @@ export function DropboxGalleryComponent() {
     } finally {
       setLoading(false)
     }
-  }, [dbx])
+  }, [dbx, hasMore, start, handleApiError])
 
   const loadImagesMetadata = useCallback(async () => {
     try {
       setLoading(true)
       let imageFiles = [] as files.FileMetadata[]
-      let metaCursor = null;
+      let metaCursor = null
 
-      // get a access token by a refresh token
+      // Get access token if not already available
       if (!dbx) {
-        const response = await fetch('https://api.dropbox.com/oauth2/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: new URLSearchParams({
-            grant_type: 'refresh_token',
-            refresh_token: process.env.NEXT_PUBLIC_DROPBOX_REFRESH_TOKEN ?? '',
-            client_id: process.env.NEXT_PUBLIC_DROPBOX_APP_KEY ?? '',
-            client_secret: process.env.NEXT_PUBLIC_DROPBOX_APP_SECRET ?? '',
-          }),
-        })
-        const data = await response.json()
-        console.log("data", data)
-        const accessToken = data.access_token
-        console.log("accessToken", accessToken)
-        dbx = new Dropbox({ accessToken })
+        const token = await getAccessToken()
+        dbx = new Dropbox({ accessToken: token })
       }
 
       while (true) {
-        let response = null;
+        let response = null
         if (metaCursor === null) {
           response = await dbx.filesListFolder({
             path: process.env.NEXT_PUBLIC_DROPBOX_FOLDER_PATH ?? '',
@@ -108,12 +126,10 @@ export function DropboxGalleryComponent() {
         ) as files.FileMetadata[]
         imageFiles = [...imageFiles, ...imageFilesI]
 
-        if (!response.result.has_more) {
-          break
-        }
-
+        if (!response.result.has_more) break
         metaCursor = response.result.cursor
       }
+
       imageFiles.sort((a, b) => a.client_modified > b.client_modified ? -1 : 1)
       imagesMetadataRef.current = imageFiles
     } catch (err) {
@@ -122,7 +138,7 @@ export function DropboxGalleryComponent() {
     } finally {
       setLoading(false)
     }
-  }, [dbx])
+  }, [dbx, getAccessToken])
 
   const initialized = useRef(false)
   useEffect(() => {
@@ -130,7 +146,7 @@ export function DropboxGalleryComponent() {
       loadImagesLazy()
       initialized.current = true
     }
-  })
+  }, [loadImagesLazy])
 
   if (error) {
     return (
@@ -151,10 +167,7 @@ export function DropboxGalleryComponent() {
     <div className="container mx-auto px-4 py-8">
       <div className="space-y-8 max-w-3xl mx-auto">
         {images.map((image, index) => (
-          <div
-            key={image.name}
-            className="w-full"
-          >
+          <div key={image.name} className="w-full">
             <div className="relative w-full">
               <Image
                 src={image.url}
@@ -164,11 +177,7 @@ export function DropboxGalleryComponent() {
                 layout="responsive"
                 className="rounded-lg shadow-lg"
                 onLoad={() => {
-                  console.log(`Loading image ${index}...`);
-                  if (index === images.length - 1) {
-                    console.log(`Loading more images...`)
-                    loadImagesLazy()
-                  }
+                  if (index === images.length - 1) loadImagesLazy()
                 }}
                 onClick={() => window.open(image.url, '_blank')}
               />
@@ -179,8 +188,7 @@ export function DropboxGalleryComponent() {
           <div className="space-y-8">
             {Array(2).fill(0).map((_, index) => (
               <div key={index} className="w-full">
-                <Skeleton className="h-64 w-full rounded-lg" />
-                <Skeleton className="h-4 w-1/2 mt-2" />
+                <Skeleton className="w-full rounded-lg" style={{ height: '360px' }} />
               </div>
             ))}
           </div>
